dd1eee5c06bde4fa91bf2a4fb6ea533be1f09f86
mgarmer2811 <163144544+mgarmer2811@users.noreply.github.com>
2025-06-08 22:53:40 +0200

    Update arreglo filtracion credenciales

--- app/src/main/java/com/mgm/data/remote/SupabaseClient.kt
+++ app/src/main/java/com/mgm/data/remote/SupabaseClient.kt
@@ -6,8 +6,8 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClient {
     val client = createSupabaseClient(
-        supabaseUrl = "https://ajfrhoegesuwjrjvsdiz.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqZnJob2VnZXN1d2pyanZzZGl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzNDE4MzksImV4cCI6MjA2MzkxNzgzOX0.4HahLmY0-iGpHjHcTeS5Xm0Zx4xrv_1jKr_Fw_MqRVA"
+        supabaseUrl = "",
+        supabaseKey = ""
     ) {
         install(Postgrest)
         install(Auth) {
